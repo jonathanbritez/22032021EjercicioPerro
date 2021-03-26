37385eef673a6a7d545b5e4bfcc007dc88c11d10
jonathanbritez <jonathanmix2016@hotmail.com>
2021-03-25 21:14:01 -0300

    Initial commit

---
+++ src/main/java/programacion/ejercicioperro/clasePerro.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package programacion.ejercicioperro;
+
+/**
+ *
+ * @author jonathan
+ */
+public class clasePerro {
+    
+    public static void main(String arg[]){
+    
+        perro animal = new perro();
+        animal.ladrar();
+    
+    }
+    
+}
---
+++ src/main/java/programacion/ejercicioperro/perro.java
@@ -0,0 +1,22 @@
+/**
+ * Crear la clase Perro, la misma debe tener 3 atributos y el método ladrar() que muestre “Guau” por consola. 
+ * Crear un objeto de esta clase y comprobar el funcionamiento
+ */
+package programacion.ejercicioperro;
+
+//Creo la clase perro
+public class perro {
+    String Nombre;
+    String Color;
+    String Tamaño;
+    
+
+//Hago el metodo ladrar
+
+    public void ladrar(){
+        
+        System.out.println("Guau");
+        
+    }    
+    
+}
